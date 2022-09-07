f47f67a8d59bd394f2d43de4dad5df0eb92c33af
NirvanaRivera <nirrivera93@gmail.com>
2022-09-07 19:30:06 -0300

    crear card del producto con contador

---
+++ src/components/ItemCount/index.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import { HStack, Button, Text, Stack, Box, Heading, Center } from '@chakra-ui/react';
+
+const ItemCount = ({ initial, stock, onAdd }) => {
+ 
+    const [count, setCount] = useState(1)
+
+    const increase = () => count < stock && setCount(count + 1);
+    const decrease = () => count > initial && setCount(count - 1);
+
+    return (
+        <HStack>
+            <Stack spacing={8} direction='row'>
+                <Box p={5} shadow='md' borderWidth='1px'>
+                    <Heading fontSize='xl' pb={3} textAlign='center'>Producto</Heading>
+                    <Center>
+                        <HStack>
+                            <Button variant='outline' colorScheme='orange' size='sm' onClick={decrease}>-</Button>
+                            <Text>{count}</Text>
+                            <Button variant='outline' colorScheme='orange' size='sm' onClick={increase}>+</Button>
+                        </HStack>
+                    </Center>
+                    <Box py={6}>
+                        <Button colorScheme='orange' size='sm'>Agregar al carrito</Button>
+                    </Box>
+                </Box>
+            </Stack>
+            
+        </HStack>
+    );
+};
+
+export default ItemCount;
--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Center, Heading } from '@chakra-ui/react';
+import ItemCount from '../ItemCount';
 
 const ItemListContainer = ({greeting}) => {
     return (
-        <Heading>
-            <Center as='ins'>Hola {greeting} ! Bienvenido</Center>
-        </Heading>
+        <>
+            <Heading>
+                <Center as='ins'>Hola {greeting} ! Bienvenido</Center>
+            </Heading>
+            <ItemCount initial={1} stock={7} onAdd={() => {}}/>
+        </>
+        
     );
 };
 
